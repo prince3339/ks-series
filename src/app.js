ebcd3de27146c12d929507e980495ed23808180a
Sabbir Ahmed Siddiquee <prince3339@ymail.com>
2019-09-26 17:19:34 +0600

    scene 2 updated

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 (function(window) {
   console.log("Project booted!!!");
   const headerHeight = document.getElementById("ks-header").clientHeight
-  document.getElementById("ks-header").style.height = headerHeight + 'px';
+  document.getElementById("ks-content").style.height = `calc(100vh - ${headerHeight}px)`;
   document.getElementById("ks-section1").style.marginTop = headerHeight + 30 + 'px';
   function gotoSection(targetId, currentId, ignoreOffset) {
     const header = document.getElementById('ks-header');
     const targetSection = document.getElementById(targetId);
     const currentSection = document.getElementById(currentId);
+    // if(targetId === 'ks-section2' || targetId === 'ks-section3') {
+    //   document.getElementById(targetId).style.marginTop =  headerHeight + 'px';
+    // }
     if (targetId !== 'ks-section1') {
       document.getElementById('ks-header').classList.add('ks-header--notitle');
     } else {
