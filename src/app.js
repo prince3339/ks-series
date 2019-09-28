0523da9848049efba02d2ad2d33eaa1b3aa4f6bd
Sabbir Ahmed Siddiquee <prince3339@ymail.com>
2019-09-29 03:18:10 +0600

    desktop ui updated

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 (function(window) {
   console.log("Project booted!!!");
   const headerHeight = document.getElementById("ks-header").clientHeight
-  document.getElementById("ks-content").style.height = "100vh";
-  document.getElementById("ks-section1").style.marginTop = headerHeight + 30 + 'px';
+  function init() {
+    document.getElementById("ks-content").style.height = "100vh";
+    document.getElementById("ks-section1").style.marginTop = headerHeight + 30 + 'px';
+  }
   function gotoSection(targetId, currentId, ignoreOffset) {
     const header = document.getElementById('ks-header');
     const targetSection = document.getElementById(targetId);
@@ -10,6 +12,16 @@
     // if(targetId === 'ks-section2' || targetId === 'ks-section3') {
     //   document.getElementById(targetId).style.marginTop =  headerHeight + 'px';
     // }
+    if(
+      targetId === 'ks-section4' ||
+      targetId === 'ks-section5' ||
+      targetId === 'ks-section6' ||
+      targetId === 'ks-section7'
+    ) {
+      const footerHeight = document.querySelector('#'+targetId + '> .ks-actions').clientHeight;
+      
+      document.querySelector(`#${targetId} svg`).style.height = `calc(100vh - ${footerHeight + headerHeight + 80}px)`;
+    }
     if (targetId !== 'ks-section1') {
       document.getElementById('ks-header').classList.add('ks-header--notitle');
     } else {
@@ -23,5 +35,10 @@
     currentSection.classList.add('fadeOut');
     currentSection.style.display = 'none';
   }
+  init();
+
+  window.addEventListener("resize", () => {
+    init();
+  });
   window.gotoSection = gotoSection;
 })(window);
